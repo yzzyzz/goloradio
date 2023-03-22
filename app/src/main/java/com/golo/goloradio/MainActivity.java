1a466b06df7240b73cded68ff7351b15aa5f8faa
yzzyzz <c1020378873@gmail.com>
2023-03-22 13:16:49 +0800

    list ok

--- app/src/main/java/com/golo/goloradio/MainActivity.java
+++ app/src/main/java/com/golo/goloradio/MainActivity.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.widget.LinearLayout;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +26,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String downdir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString();
-        //String StringFileContent=new FileInputStream(downdir+"/aa.m3u").toString();
-        Button currentSelectItem = findViewById(R.id.button_zgzs);
-        String currentUrl = "http://ngcdn016.cnr.cn/live/gsgljtgb/index.m3u8?";
-        currentSelectItem.setOnClickListener(new PlayM3uRadio(currentUrl));
+        // 1、获取资源列表
+        List playList = getUrlListFromRes();
+        // 2、生成buttom 列表
+        // 3、设置点击播放
+        LinearLayout layout = findViewById(R.id.radiolist);
+        for(int i=0;i<playList.size();i++)
+        {
+            Button radioItem = new Button(this);
+            String[] datainfo = (String[]) playList.get(i);
+            Log.i("读取的信息", "onCreate: "+ datainfo[0]);
+            radioItem.setText(datainfo[0]);
+            radioItem.setOnClickListener(new PlayM3uRadio(datainfo[1]));
+            layout.addView(radioItem);
+        }
+    }
+    public List getUrlListFromRes(){
+        List ret = new ArrayList();
 
+        InputStream inputStream = getResources().openRawResource(R.raw.radio);
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        StringBuffer sb = new StringBuffer("");
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                Log.i("line info", "getUrlListFromRes: "+line);
+                String[] split=line.split(",");
+                if(split.length == 2){
+                    ret.add(split);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  ret;
     }
 
-    static class PlayM3uRadio implements View.OnClickListener{
-        private String playUrl ;
+    static class PlayM3uRadio implements View.OnClickListener {
+        private String playUrl;
         MediaPlayer mediaPlayer = new MediaPlayer();
-        public PlayM3uRadio(String url){
+
+        public PlayM3uRadio(String url) {
             this.playUrl = url;
-            Log.i("url", "PlayM3uRadio: "+this.playUrl);
+            Log.i("url", "PlayM3uRadio: " + this.playUrl);
         }
+
         @Override
-        public void onClick(View view){
+        public void onClick(View view) {
 
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
@@ -41,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                 mediaPlayer.setDataSource(this.playUrl);
                 mediaPlayer.setLooping(true);
-                mediaPlayer.setVolume(50,50);
+                mediaPlayer.setVolume(50, 50);
 
 
                 //mediaPlayer.setOnPreparedListener(this);
@@ -52,11 +98,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
-        public boolean isPaused() {
-            return !mediaPlayer.isPlaying();
-        }
     }
-
 }
 
